e496f77c679fdc0e3c04a6791ebf763d11a18bbb
Daniel Lozano <mastermiw@192.168.7.31>
2015-03-13 17:43:21 +0100

    Creados atributos y añadido en persistence vote4topic

--- src/main/java/models/entities/vote.java
+++ src/main/java/models/entities/vote.java
@@ -1,5 +1,6 @@
 package models.entities;
 
 public class vote {
+	
 
 }
--- src/main/java/models/entities/vote4topic.java
+++ src/main/java/models/entities/vote4topic.java
@@ -1,5 +1,26 @@
 package models.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
 public class vote4topic {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String vote;
+	private String topic;
+	
+	public vote4topic(){}
+	
+	public vote4topic(String vote,String topic){
+		super();
+        this.topic = topic;
+        this.vote = vote;
+	}
 
 }
