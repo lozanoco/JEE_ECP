ba723fb003633730a52e22d1c28deafd22f82fe2
Daniel Lozano <mastermiw@192.168.7.31>
2015-03-13 17:47:20 +0100

    atributos de vote y persistence xml

--- src/main/java/models/entities/vote.java
+++ src/main/java/models/entities/vote.java
@@ -1,6 +1,28 @@
 package models.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import models.utils.levelEducation;
+
+@Entity
 public class vote {
+	@Id
+	@GeneratedValue
+	private int id;
 	
+	private String ip;
+	private int score;
+	private levelEducation education;
+	
+	public vote(){}
+	
+	public vote(String ip,int score,levelEducation education){
+		super();
+        this.score = score;
+        this.ip = ip;
+        this.education=education;
+	}
 
 }
