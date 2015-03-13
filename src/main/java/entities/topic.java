5dc62a05f7cd1422ae850ae15366c562440591b9
Daniel Lozano <mastermiw@192.168.7.31>
2015-03-13 17:28:27 +0100

    Atributos y constructor de topic definidos

--- src/main/java/entities/topic.java
+++ src/main/java/entities/topic.java
@@ -1,5 +1,25 @@
 package entities;
 
-public class topic {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class topic {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String name;
+	private String question;
+	
+	public topic(){}
+	
+	public topic(String name,String question){
+		super();
+        this.name = name;
+        this.question = question;
+	}
+	
 }
