adc8d5f9edfaf0c545441b58fab4eaa61f1d6888
Daniel Lozano <mastermiw@192.168.7.31>
2015-03-13 17:36:11 +0100

    Añadir persistence, meter topic dentro y reestructuracion de paquetes

--- src/main/java/models/entities/topic.java
+++ src/main/java/models/entities/topic.java
@@ -1,4 +1,4 @@
-package entities;
+package models.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/models/utils/levelEducation.java
+++ src/main/java/models/utils/levelEducation.java
@@ -1,4 +1,4 @@
-package utils;
+package models.utils;
 
 public enum levelEducation {
 	PRIMARY,SECONDARY,HIGHER
