032cbc4fe2b71b899eab6e4fe28d523478a6a1b1
Daniel Lozano <mastermiw@192.168.7.31>
2015-03-13 17:53:26 +0100

    genrados get/set/constructor de topic automaticos

--- src/main/java/models/entities/topic.java
+++ src/main/java/models/entities/topic.java
@@ -10,16 +10,42 @@ public class topic {
 	@Id
 	@GeneratedValue
 	private int id;
-	
+
 	private String name;
 	private String question;
 	
+	public topic(int id, String name, String question) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.question = question;
+	}
+	
 	public topic(){}
 	
-	public topic(String name,String question){
-		super();
-        this.name = name;
-        this.question = question;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
 	}
 	
 }
