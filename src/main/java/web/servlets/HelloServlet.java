d1b2b4afdf6df17ef4924dff605053583137fe18
Dani <Dani@Home>
2015-04-02 13:33:50 +0200

    Generado el servlet y la prueba de funcionamiento

---
+++ src/main/java/web/servlets/HelloServlet.java
@@ -0,0 +1,38 @@
+package web.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello_servlet
+ */
+@WebServlet("/JEE_ECP/jsp/home")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("/JEE_ECP/jsp/home con metodo GET");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("/JEE_ECP/jsp/home con metodo Post");
+	}
+
+}
