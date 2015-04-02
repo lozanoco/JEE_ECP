fc77a2eaf54796001006f30bbe5fccf5d7f8289b
Dani <Dani@Home>
2015-04-02 16:23:21 +0200

    Generado el bean de votar con la carga de topics para su selección

--- src/main/java/views/beans/VoteBean.java
+++ src/main/java/views/beans/VoteBean.java
@@ -1,12 +1,31 @@
 package views.beans;
 
-import java.io.Serializable;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+
+import persistence.models.daos.jpa.DaoJpaFactory;
+import persistence.models.entities.Topic;
+
 
 public class VoteBean extends GenericBean{
 
+	private static final long serialVersionUID = 1L;
+	
+	List<Topic> topics;
+	
+	public VoteBean() {
+		topics = chargeTopics();
+	}
+
+	private List<Topic> chargeTopics() {
+		return DaoJpaFactory.getFactory().getTopicDao().findAll();
+	}
+
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		LogManager.getLogger(VoteBean.class).debug(
+                "Recupera temas del Negocio");
+        this.topics = null;
 		
 	}
 
