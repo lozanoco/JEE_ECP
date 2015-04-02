c5a3acc9d58a4781f21d7a519cdbf743d56a5229
Dani <Dani@Home>
2015-04-02 16:29:15 +0200

    Generada Factoria de controladores y controlador de votaciones que hereda de esta

---
+++ src/main/java/controllers/VoteController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+public class VoteController extends ControllerFactory{
+
+	@Override
+	public VoteController getVoteController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
