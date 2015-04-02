9f8ee1005e3273de2382391d5ab3870b59bf0094
Dani <Dani@Home>
2015-04-02 16:10:05 +0200

    Rellenada la implemantacion del GenericBean de la que heredaran los beans

--- src/main/java/views/beans/GenericBean.java
+++ src/main/java/views/beans/GenericBean.java
@@ -1,5 +1,12 @@
 package views.beans;
 
-public class GenericBean {
+import java.io.Serializable;
 
+public abstract class GenericBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public abstract void update();
+    
+    public abstract String process();
 }
--- src/main/java/views/beans/VoteBean.java
+++ src/main/java/views/beans/VoteBean.java
@@ -1,5 +1,19 @@
 package views.beans;
 
-public class VoteBean {
+import java.io.Serializable;
+
+public class VoteBean extends GenericBean{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String process() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
