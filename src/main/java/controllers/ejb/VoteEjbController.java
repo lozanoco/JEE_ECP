488cadf9af8deb15d5949a1d062d26d4e8c67b51
Dani <Dani@Home>
2015-04-02 16:53:44 +0200

    Generada la implementacion de VoteEjbController

--- src/main/java/controllers/ejb/VoteEjbController.java
+++ src/main/java/controllers/ejb/VoteEjbController.java
@@ -2,6 +2,9 @@ package controllers.ejb;
 
 import java.util.List;
 
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.TopicDao;
+import persistence.models.daos.jpa.DaoJpaFactory;
 import persistence.models.entities.Topic;
 import persistence.models.entities.Vote;
 import controllers.VoteController;
@@ -10,20 +13,25 @@ public class VoteEjbController implements VoteController{
 
 	@Override
 	public List<Topic> obtainTopics() {
-		// TODO Auto-generated method stub
-		return null;
+		DaoFactory.setFactory(new DaoJpaFactory());
+        TopicDao topicDao = DaoFactory.getFactory().getTopicDao();
+		return topicDao.findAll();
 	}
 
 	@Override
 	public Topic findTopic(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		DaoFactory.setFactory(new DaoJpaFactory());
+        TopicDao topicDao = DaoFactory.getFactory().getTopicDao();
+		return topicDao.read(id);
 	}
 
 	@Override
 	public void vote(Topic topic, Vote vote) {
-		// TODO Auto-generated method stub
-		
+		DaoFactory.setFactory(new DaoJpaFactory());
+        TopicDao topicDao = DaoFactory.getFactory().getTopicDao();
+        Topic topicPersist = topicDao.read(topic.getId());
+        topicPersist.setVote(vote);
+        topicDao.update(topicPersist);
 	}
 
 }
