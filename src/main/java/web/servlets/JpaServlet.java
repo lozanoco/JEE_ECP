d51d62625b33ab8ea6a52471a115a97c2fc60f1c
Dani <Dani@Home>
2015-04-02 14:55:40 +0200

    Generado switch en el dispatcher con las distintas acciones.
    Creado VoteBean

--- src/main/java/web/servlets/JpaServlet.java
+++ src/main/java/web/servlets/JpaServlet.java
@@ -9,33 +9,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import views.beans.VoteBean;
+
 /**
  * Servlet implementation class Hello_servlet
  */
 @WebServlet(urlPatterns={"/jsp/*"})
 public class JpaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public JpaServlet() {
-        super();
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public JpaServlet() {
+		super();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getPathInfo().substring(1);
-//		String view = "Home";
-		if(action.equals("Vote")){	
-			String web = "<html><head><title>Vote</title></head>"
-	                + "<body> <h1>Entra en voto</h1>" + "</body></html>";
-	        response.setContentType("text/html");
-	        PrintWriter out = response.getWriter();
-	        out.println(web);
+		String view = "Home";
+		switch (action) {
+		case "Votar":
+			VoteBean voteBean = new VoteBean();
 
+			break;
+			//		case "VerVotaciones":
+			//			ViewVotesBean verVotacionesBean = new ViewVotesBean();
+			//
+			//			break;
+			//		case "AniadirTema":
+			//			AddTopicBean aniadirTemaBean = new AddTopicBean();
+			//
+			//			break;
+			//		case "EliminarTema":
+			//			DeleteTopicBean eliminarTemaBean = new DeleteTopicBean();
+			//
+			//			break;
+		default:
+			view = "Home";
 		}
 	}
 
